6d60eb08403e86e4db44b73e1d0d6b525c96b474
Haertel <otto.haertel@siemens.com>
2024-06-28 13:23:04 +0200

    Encode rudimentär+

--- RailFenceCipher/RailFenceCipherer.cs
+++ RailFenceCipher/RailFenceCipherer.cs
@@ -4,6 +4,21 @@
     {
         public string Encode(string StringToEncode, int numberOfRails)
         {
+            int currentVericalPosition=0;
+            //Array kann zwei Demensional deterministisch von Anfang an vorbelegt werden
+            //Jedes Array muss so groß sein, wie die länge des Strings
+
+            List<string[]> reuslt = new List<string[]>();
+            for(int i=0; i< numberOfRails; i++)
+            {
+                string[]temp = new string[StringToEncode.Length];
+                reuslt.Add(temp);
+            }
+            //currentVericalPosition = stringToEncode[index]
+            do
+            {
+                currentVericalPosition++;
+            } while(currentVericalPosition<StringToEncode.Length);
             return "";
         }
     }
